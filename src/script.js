6f3ff66cda30af002f61a8ac4ac123683cfed5ca
Imran Ziad <imranziad@gmail.com>
2020-06-26 00:04:44 +0600

    set/get app id, key to localStorage

--- src/script.js
+++ src/script.js
@@ -14,32 +14,74 @@ $(document).ready(function () {
         }
     });
 
-    
-});
+    var Kairos = {
+        appId: null,
+        appKey: null,
 
-var kairosRcognize = function() {
-    var form = new FormData();
-    form.append("image", fileInput.files[0], "/C:/Users/Imran Ziad/Desktop/ML/NTB/datasets/download (1).png");
-    form.append("gallery_name", "People");
-    form.append("threshold", "0.60");
-    form.append("max_num_results", "4");
-
-    var settings = {
-        "url": "https://api.kairos.com/recognize",
-        "method": "POST",
-        "timeout": 0,
-        "headers": {
-            "Content-Type": "application/json",
-            "app_id": "05f64948",
-            "app_key": "7d5e55812ccd8393caaedf118dafd903"
+        setAppId: function(appId) {
+            this.appId = appId;
+            localStorage.setItem("kairos_app_id", appId);
         },
-        "processData": false,
-        "mimeType": "multipart/form-data",
-        "contentType": false,
-        "data": form
+        getAppId: function() {
+            if(this.appId == null)
+                this.appId = localStorage.getItem("kairos_app_id");
+            return this.appId;
+        },
+        setAppKey: function(appKey) {
+            this.appKey = appKey;
+            localStorage.setItem("kairos_app_key", appKey);
+        },
+        getAppKey: function() {
+            if(this.appKey == null)
+                this.appKey = localStorage.getItem("kairos_app_key");
+            return this.appKey;
+        },
     };
 
-    $.ajax(settings).done(function (response) {
-        console.log(response);
+    $("#kairosAppIdSet").click(function() {
+        var appId = $("#inputKairosAppId").val();
+        Kairos.setAppId(appId);
+        $("#inputKairosAppId").val("");
     });
-}
+
+    $("#kairosAppIdGet").click(function() {
+        $("#inputKairosAppId").val(Kairos.getAppId());
+    });
+
+    $("#kairosAppKeySet").click(function() {
+        var appKey = $("#inputKairosAppKey").val();
+        Kairos.setAppKey(appKey);
+        $("#inputKairosAppKey").val("");
+    });
+
+    $("#kairosAppKeyGet").click(function() {
+        $("#inputKairosAppKey").val(Kairos.getAppKey());
+    });
+    
+    var kairosRcognize = function() {
+        var form = new FormData();
+        form.append("image", fileInput.files[0], "/C:/Users/Imran Ziad/Desktop/ML/NTB/datasets/download (1).png");
+        form.append("gallery_name", "People");
+        form.append("threshold", "0.60");
+        form.append("max_num_results", "4");
+    
+        var settings = {
+            "url": "https://api.kairos.com/recognize",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "Content-Type": "application/json",
+                "app_id": "05f64948",
+                "app_key": "7d5e55812ccd8393caaedf118dafd903"
+            },
+            "processData": false,
+            "mimeType": "multipart/form-data",
+            "contentType": false,
+            "data": form
+        };
+    
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+        });
+    }
+});
