fb6302d55f85aa0c18653c6cf9470e68b8dc963e
Imran Ziad <imranziad@gmail.com>
2020-06-29 14:16:08 +0600

    added test error responses

--- src/script.js
+++ src/script.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         onComplete: function (imageData, isCompleted, src) {
             let imageId = src.attr("id");
             compressedImages[imageId] = imageData;
-            let compressedImageDiv = $('#'+imageId+'Compressed');
+            let compressedImageDiv = $('#' + imageId + 'Compressed');
             compressedImageDiv.children(".img-body").html($("<img/>", {
                 src: imageData,
                 height: 200,
@@ -19,15 +19,6 @@ $(document).ready(function () {
                 .append("Original Image Size: " + Math.round(src[0].files[0].size / 1024) + " KB");
             compressedImageDiv.children(".img-body")
                 .append("<br>Compressed Blob Size: " + Math.round((new Blob([imageData])).size / 1024) + " KB");
-            /*
-            compressedImageDiv.html($("<img/>", {
-                src: imageData,
-                height: 200,
-                class: 'd-block'
-            }));
-            compressedImageDiv.append("Original Image Size: " + Math.round(src[0].files[0].size / 1024) + " KB");
-            compressedImageDiv.append("<br>Compressed Blob Size: " + Math.round((new Blob([imageData])).size / 1024) + " KB");
-            */
         },
         onFailure: function (message) {
             alert(message);
@@ -41,32 +32,32 @@ $(document).ready(function () {
         threshold: 0.60,
         maxNumResults: 10,
 
-        setAppId: function(appId) {
+        setAppId: function (appId) {
             this.appId = appId;
             localStorage.setItem("kairos_app_id", appId);
         },
-        getAppId: function() {
-            if(this.appId == null)
+        getAppId: function () {
+            if (this.appId == null)
                 this.appId = localStorage.getItem("kairos_app_id");
             return this.appId;
         },
-        setAppKey: function(appKey) {
+        setAppKey: function (appKey) {
             this.appKey = appKey;
             localStorage.setItem("kairos_app_key", appKey);
         },
-        getAppKey: function() {
-            if(this.appKey == null)
+        getAppKey: function () {
+            if (this.appKey == null)
                 this.appKey = localStorage.getItem("kairos_app_key");
             return this.appKey;
         },
-        recognize: function(image, imageName, onFinished) {
+        recognize: function (image, imageName, onFinished) {
             var form = new FormData();
             let blob = new Blob([image]);
             form.append("image", blob, imageName);
             form.append("gallery_name", this.galleryName);
             form.append("threshold", this.threshold);
             form.append("max_num_results", this.maxNumResults);
-        
+
             var settings = {
                 "url": "https://api.kairos.com/recognize",
                 "method": "POST",
@@ -79,50 +70,60 @@ $(document).ready(function () {
                 "contentType": false,
                 "data": form
             };
-        
+
             $.ajax(settings).done(onFinished);
         }
     };
 
-    $("#kairosRecognize").click(function() {
-        Kairos.recognize(compressedImages["kairosImageRecognize"], "photo", function(res) {
+    $("#kairosRecognize").click(function () {
+        $('#kairosImageRecognizeCompressed').children(".img-res")
+            .html("Tags: Searching...");
+
+        Kairos.recognize(compressedImages["kairosImageRecognize"], "photo", function (res) {
             console.log(res);
             let compressedImageDiv = $('#kairosImageRecognizeCompressed');
-            var candidates = filterCandidates(res);
-            console.log(candidates);
-            compressedImageDiv.children(".img-res").html("Tags:")
-            candidates.forEach(p => {
-                compressedImageDiv.children(".img-res").append("<br>" + p.name + " (" + p.confidence + "),")
-            });
+            compressedImageDiv.children(".img-res").html("Tags:");
+
+            if (res.hasOwnProperty("Errors")) {
+                compressedImageDiv.children(".img-res").append('<br><span class="text-danger">' + res["Errors"][0]["Message"]+'</span>');
+            } else {
+                var candidates = filterCandidates(res);
+                console.log(candidates);
+                candidates.forEach(p => {
+                    compressedImageDiv.children(".img-res").append("<br>" + p.name + " (" + p.confidence + "),")
+                });
+                if(candidates.length === 0)
+                    compressedImageDiv.children(".img-res").append("<br>No matches found");
+            }
         });
     });
 
-    $("#kairosAppIdSet").click(function() {
+    $("#kairosAppIdSet").click(function () {
         var appId = $("#inputKairosAppId").val();
         Kairos.setAppId(appId);
         $("#inputKairosAppId").val("");
     });
 
-    $("#kairosAppIdGet").click(function() {
+    $("#kairosAppIdGet").click(function () {
         $("#inputKairosAppId").val(Kairos.getAppId());
     });
 
-    $("#kairosAppKeySet").click(function() {
+    $("#kairosAppKeySet").click(function () {
         var appKey = $("#inputKairosAppKey").val();
         Kairos.setAppKey(appKey);
         $("#inputKairosAppKey").val("");
     });
 
-    $("#kairosAppKeyGet").click(function() {
+    $("#kairosAppKeyGet").click(function () {
         $("#inputKairosAppKey").val(Kairos.getAppKey());
     });
 
-    var filterCandidates = function(data) {
+    var filterCandidates = function (data) {
         let candidates = [];
 
         // merge all the candidates in one list
         data.images.forEach(image => {
-            if(image.candidates != null)
+            if (image.candidates != null)
                 candidates = candidates.concat(image.candidates);
         });
         let result = candidates.map((item) => {
@@ -139,8 +140,6 @@ $(document).ready(function () {
         result = result.filter((item, index, self) =>
             index === self.findIndex(t => item.name === t.name)
         );
-
-        console.log(result);
         return result;
     };
 
